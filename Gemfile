76c6e0e9020e6952a34ad7844af7f78bb8cc67d5
vvdpzz <vvdpzz@gmail.com>
2012-01-01 02:34:21 +0800

    update login page

--- Gemfile
+++ Gemfile
@@ -2,9 +2,6 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.1.3'
 
-# Bundle edge Rails instead:
-# gem 'rails',     :git => 'git://github.com/rails/rails.git'
-
 gem 'mysql2'
 gem 'omniauth', '>= 1.0.0'
 gem 'omniauth-github'
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.where(:provider => auth['provider'], 
                       :uid => auth['uid']).first || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    redirect_to root_url, :notice => 'Signed in!'
+    redirect_to user, :notice => 'Signed in!'
   end
 
   def destroy
