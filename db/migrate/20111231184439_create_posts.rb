720cc445264167b64a9dc341c1686fd95d7d1cec
vvdpzz <vvdpzz@gmail.com>
2012-01-04 10:09:54 +0800

    rm some fields

--- db/migrate/20111231184439_create_posts.rb
+++ db/migrate/20111231184439_create_posts.rb
@@ -3,12 +3,8 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.references :user
       t.string :text, :limit => 560
-      t.string :source, :limit => 200
-      t.boolean :truncated
       t.integer :in_reply_to_post_id
       t.integer :in_reply_to_user_id
-      t.integer :favorited
-      t.string :in_reply_to_nickname, :limit => 30
       
       t.datetime :created_at
     end
